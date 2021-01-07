beaa7ad0702414b362e8efc60d84df5cf17e9b0b
maxcramer <maximiliancramer@yahoo.com>
2021-01-07 13:07:01 +0000

    header styling complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './Layout/Layout';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        
+        <Header />
       </Layout>
 
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <img src="https://green.cdn.energy/branding/logo-r.svg" alt="Green Logo"/>
+            <h1>Example Login Screen</h1>
+            <p>Getting started with Green.</p>
+        </div>
+    )
+};
+
+export default Header;
