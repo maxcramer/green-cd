25c16c623fee26326315d14e6e20fc8dd8ac99e3
maxcramer <maximiliancramer@yahoo.com>
2021-01-07 14:29:52 +0000

    check input loading checked, uses state to allow user to uncheck if wished

--- src/components/EmailInput/EmailInput.js
+++ src/components/EmailInput/EmailInput.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import './EmailInput.scss'
-;
-const EmailInput = () => {
-    return (
-        <div className="Email">
-            <p>Email Address</p>
-            <input className="email-input" type="text" />
-            <div className="remember-device">
-                <input type="checkbox" checked/>
-                <p>Remember this device</p>
+import './EmailInput.scss';
+
+class EmailInput extends Component {
+    state = {
+        checked: true
+    }
+
+
+    handleChecked = () => {
+        this.handleChecked = this.handleChecked.bind(this);
+        this.setState({checked: !this.state.checked})
+    }
+
+    render() {
+        return (
+            <div className="Email">
+                <p>Email Address</p>
+                <input className="email-input" type="text" />
+                <div className="remember-device">
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChecked}/>
+                    <p>Remember this device</p>
+                </div>
             </div>
-        </div>
-    );
+        );
+    
+    }
 };
 
 export default EmailInput;
